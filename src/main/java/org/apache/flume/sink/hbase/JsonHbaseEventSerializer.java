b00aaaf44f392f4bba1f5109b2af16a53c9d09d9
zhoubh <zhoubh@asiainfo.com>
2016-09-02 13:55:54 +0800

    rowKey desigh  add send_number into it . add the last number and send_number into zhe rowkey.

--- src/main/java/org/apache/flume/sink/hbase/JsonHbaseEventSerializer.java
+++ src/main/java/org/apache/flume/sink/hbase/JsonHbaseEventSerializer.java
@@ -41,91 +41,111 @@ import com.google.gson.JsonParser;
 
 public class JsonHbaseEventSerializer implements HbaseEventSerializer {
 
-	private static final String CHARSET_CONFIG = "charset";
-	private static final String CHARSET_DEFAULT = "UTF-8";
+    private static final String CHARSET_CONFIG = "charset";
+    private static final String CHARSET_DEFAULT = "UTF-8";
 
-	protected static final AtomicLong nonce = new AtomicLong(0);
+    protected static final AtomicLong nonce = new AtomicLong(0);
 
-	protected byte[] cf;
-	private byte[] payload;
-	private Map<String, String> headers;
-	private Charset charset;
+    protected byte[] cf;
+    private byte[] payload;
+    private Map<String, String> headers;
+    private Charset charset;
+    //add by zhoubh. put zhe send_number into RowKey.
+    private String send_number;
 
-	@Override
-	public void configure(Context context) {
-		charset = Charset.forName(context.getString(CHARSET_CONFIG, CHARSET_DEFAULT));
-	}
+    @Override
+    public void configure(Context context) {
+        charset = Charset.forName(context.getString(CHARSET_CONFIG, CHARSET_DEFAULT));
+    }
 
-	@Override
-	public void configure(ComponentConfiguration conf) {
-	}
+    @Override
+    public void configure(ComponentConfiguration conf) {
+    }
 
-	@Override
-	public void initialize(Event event, byte[] columnFamily) {
-		this.headers = event.getHeaders();
-		this.payload = event.getBody();
-		this.cf = columnFamily;
-	}
-	
-	protected byte[] getRowKey(Calendar cal) {
-		String rowKey = String.format("%s%s", cal.getTimeInMillis(),nonce.getAndIncrement());
-		return rowKey.getBytes(charset);
-	}
+    @Override
+    public void initialize(Event event, byte[] columnFamily) {
+        this.headers = event.getHeaders();
+        this.payload = event.getBody();
+        this.cf = columnFamily;
+    }
 
-	protected byte[] getRowKey() {
-		return getRowKey(Calendar.getInstance());
-	}
+    protected byte[] getRowKey_2(Calendar cal, String s) {
+        String rowKey = String.format("%s-%s-%s", s, cal.getTimeInMillis(), nonce.getAndIncrement());
+        return rowKey.getBytes(charset);
+    }
 
-	@Override
-	public List<Row> getActions() throws FlumeException {
-		List<Row> actions = Lists.newArrayList();
-		try {
-		
-			byte[] rowKey = getRowKey();
-			
-			Put put = new Put(rowKey);
-			
-			JsonElement element=new JsonParser().parse(new String(payload));
-			
-			if (element.isJsonNull()) {
-				return actions;
-			}
-			
-			if (!element.isJsonObject()) {
-				return actions;
-			}
-			
-			JsonObject jsonObject=element.getAsJsonObject();
-			
-			Set<Entry<String, JsonElement>> sets = jsonObject.entrySet();
+    protected byte[] getRowKey_2(String s) {
+        return getRowKey_2(Calendar.getInstance(), s);
+    }
 
-			sets.remove("headers");
-	
-			for (Entry<String, JsonElement> entry : sets) {
-				String xcontent=entry.getValue().toString();
-				if (xcontent.startsWith("\"")&&xcontent.endsWith("\"")) {
-					xcontent=xcontent.substring(1,xcontent.length()-1);
-				}
-				put.addColumn(cf, entry.getKey().getBytes(charset), xcontent.getBytes(charset));
-			}
-			
-	        for (Map.Entry<String, String> entry : headers.entrySet()) {
-				put.addColumn(cf, entry.getKey().getBytes(charset), entry.getValue().getBytes(charset));
-			}
-			
-			actions.add(put);
-		} catch (Exception e) {
-			throw new FlumeException(e.getMessage(), e);
-		}
-		return actions;
-	}
+    protected byte[] getRowKey(Calendar cal) {
+        String rowKey = String.format("%s-%s", cal.getTimeInMillis(), nonce.getAndIncrement());
+        return rowKey.getBytes(charset);
+    }
 
-	@Override
-	public List<Increment> getIncrements() {
-		return Lists.newArrayList();
-	}
+    protected byte[] getRowKey() {
+        return getRowKey(Calendar.getInstance());
+    }
 
-	@Override
-	public void close() {
-	}
+    @Override
+    public List<Row> getActions() throws FlumeException {
+        List<Row> actions = Lists.newArrayList();
+
+        try {
+
+            byte[] rowKey = getRowKey();
+            Put put = new Put(rowKey);
+
+            JsonElement element = new JsonParser().parse(new String(payload));
+
+            if (element.isJsonNull()) {
+                return actions;
+            }
+
+            if (!element.isJsonObject()) {
+                return actions;
+            }
+
+            JsonObject jsonObject = element.getAsJsonObject();
+
+            Set<Entry<String, JsonElement>> sets = jsonObject.entrySet();
+            sets.remove("headers");
+
+            for (Entry<String, JsonElement> entry : sets) {
+                String xcontent = entry.getValue().toString();
+                if (xcontent.startsWith("\"") && xcontent.endsWith("\"")) {
+                    xcontent = xcontent.substring(1, xcontent.length() - 1);
+                }
+                // add by zhoubh
+                if (entry.getKey().equals("send_number")) {
+                    String rowKey_1 = xcontent.substring(xcontent.length() - 1, xcontent.length()) + xcontent;
+                    byte[] rowKey2 = getRowKey_2(rowKey_1);
+                    put = new Put(rowKey2);
+                }
+                // add by zhoubh
+
+                put.addColumn(cf, entry.getKey().getBytes(charset), xcontent.getBytes(charset));
+
+            }
+
+
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                put.addColumn(cf, entry.getKey().getBytes(charset), entry.getValue().getBytes(charset));
+            }
+
+            actions.add(put);
+        } catch (Exception e) {
+            throw new FlumeException(e.getMessage(), e);
+        }
+        return actions;
+    }
+
+    @Override
+    public List<Increment> getIncrements() {
+        return Lists.newArrayList();
+    }
+
+    @Override
+    public void close() {
+    }
 }
